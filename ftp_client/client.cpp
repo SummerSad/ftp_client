8f732f882d4532180617ebcb7e740947d3336e0a
Tran Hau <hauvipapro@gmail.com>
2018-05-02 17:44:27 +0700

    move print reply code to another src file for easy maintain. Add quit, exit, help, ? command

--- ftp_client/client.cpp
+++ ftp_client/client.cpp
@@ -1,6 +1,7 @@
 // TODO hoan thanh print_reply_code (con mot so code chua xong)
 // TODO nhap password ma khong hien len terminal
 
+#include "reply.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <winsock2.h>
@@ -16,10 +17,11 @@
  */
 int ftp_login(SOCKET connect_SOCKET);
 
-/* Print full message from server
- * by reply_code, follow RFC 959
+/* User request exit connection from client
+ * return 0 if fail,
+ * otherwise return reply_code
  */
-void print_reply_code(int reply_code);
+int handle_ftp_exit(SOCKET connect_SOCKET);
 
 int main(int argc, char *argv[])
 {
@@ -85,6 +87,22 @@ int main(int argc, char *argv[])
 		WSACleanup();
 		printf("Login error\n");
 		return 0;
+	}
+
+	// Read commands from user
+	char input[BUFLEN];
+	while (1) {
+		printf("ftp> ");
+		fgets(input, BUFLEN, stdin);
+		input[strcspn(input, "\n")] = 0; // remove trailing '\n'
+		if (strcmp(input, "?") == 0 || strcmp(input, "help") == 0)
+			printf("quit\texit\t\n");
+		else if (strcmp(input, "quit") == 0 ||
+			 strcmp(input, "exit") == 0) {
+			handle_ftp_exit(connect_SOCKET);
+			break;
+		} else
+			printf("Command not found. Please type ? or help\n");
 	}
 
 	// Cleanup
@@ -184,118 +202,36 @@ int ftp_login(SOCKET connect_SOCKET)
 	return reply_code;
 }
 
-void print_reply_code(int reply_code)
+int handle_ftp_exit(SOCKET connect_SOCKET)
 {
-	if (reply_code == 110)
-		printf(
-		    "%d Restart marker reply.\n"
-		    "    In this case, the text is exact and not left to the\n"
-		    "    particular implementation; it must read :\n"
-		    "    \tMARK yyyy = mmmm\n"
-		    "    Where yyyy is User - process data stream marker, and "
-		    "mmmm\n"
-		    "    server's equivalent marker (note the spaces between "
-		    "markers\n"
-		    "    and \"=\").\n",
-		    reply_code);
+	char buf[BUFLEN];
+	int len_temp;
+	int reply_code;
+	int status;
 
-	else if (reply_code == 120)
-		printf("%d Service ready in nnn minutes.\n", reply_code);
-	else if (reply_code == 125)
-		printf("%d Data connection already open; transfer starting.\n",
-		       reply_code);
-	else if (reply_code == 150)
-		printf("%d File status okay; about to open data connection.\n",
-		       reply_code);
-	else if (reply_code == 200)
-		printf("%d Command okay.\n", reply_code);
-	else if (reply_code == 202)
-		printf(
-		    "%d Command not implemented, superfluous at this site.\n",
-		    reply_code);
-	else if (reply_code == 211)
-		printf("%d System status, or system help reply.\n", reply_code);
-	else if (reply_code == 212)
-		printf("%d Directory status.\n", reply_code);
-	else if (reply_code == 213)
-		printf("%d File status.\n", reply_code);
-	else if (reply_code == 214)
-		printf("%d Help message.\n"
-		       "    On how to use the server or the meaning of a "
-		       "particular\n"
-		       "    non - standard command.This reply is useful only "
-		       "to the\n"
-		       "    human user.\n",
-		       reply_code);
-	else if (reply_code == 215)
-		printf("%d NAME system type.\n"
-		       "    Where NAME is an official system name from the "
-		       "list in the\n"
-		       "    Assigned Numbers document.\n",
-		       reply_code);
-	else if (reply_code == 220)
-		printf("%d Service ready for new user.\n", reply_code);
-	else if (reply_code == 221)
-		printf("%d\n", reply_code);
-	else if (reply_code == 225)
-		printf("%d\n", reply_code);
-	else if (reply_code == 226)
-		printf("%d\n", reply_code);
-	else if (reply_code == 227)
-		printf("%d\n", reply_code);
-	else if (reply_code == 230)
-		printf("%d User logged in, proceed.\n", reply_code);
-	else if (reply_code == 250)
-		printf("%d Requested file action okay, completed.\n",
-		       reply_code);
-	else if (reply_code == 257)
-		printf("%d\n", reply_code);
-	else if (reply_code == 331)
-		printf("%d User name okay, need password.\n", reply_code);
-	else if (reply_code == 332)
-		printf("%d Need account for login.\n", reply_code);
-	else if (reply_code == 350)
-		printf("%d\n", reply_code);
-	else if (reply_code == 421)
-		printf("%d Service not available, closing control connection.\n"
-		       "    This may be a reply to any command if the service "
-		       "knows it\n"
-		       "    must shut down.\n",
-		       reply_code);
-	else if (reply_code == 425)
-		printf("%d\n", reply_code);
-	else if (reply_code == 426)
-		printf("%d\n", reply_code);
-	else if (reply_code == 450)
-		printf("%d\n", reply_code);
-	else if (reply_code == 451)
-		printf("%d\n", reply_code);
-	else if (reply_code == 451)
-		printf("%d\n", reply_code);
-	else if (reply_code == 500)
-		printf("%d Syntax error, command unrecognized.\n"
-		       "    This may include errors such as command line too "
-		       "long.\n",
-		       reply_code);
-	else if (reply_code == 501)
-		printf("%d Syntax error in parameters or arguments.\n",
-		       reply_code);
-	else if (reply_code == 502)
-		printf("%d\n", reply_code);
-	else if (reply_code == 503)
-		printf("%d\n", reply_code);
-	else if (reply_code == 504)
-		printf("%d\n", reply_code);
-	else if (reply_code == 530)
-		printf("%d Not logged in.\n", reply_code);
-	else if (reply_code == 532)
-		printf("%d\n", reply_code);
-	else if (reply_code == 550)
-		printf("%d\n", reply_code);
-	else if (reply_code == 551)
-		printf("%d\n", reply_code);
-	else if (reply_code == 552)
-		printf("%d\n", reply_code);
-	else if (reply_code == 553)
-		printf("%d\n", reply_code);
+	char quit_msg[] = "QUIT\r\n";
+	strncpy_s(buf, quit_msg, strlen(quit_msg));
+	status = send(connect_SOCKET, buf, strlen(buf), 0);
+	if (status == SOCKET_ERROR) {
+		printf("send() error %d\n", WSAGetLastError());
+	}
+	memset(buf, 0, BUFLEN);
+
+	// Check server recieving quit msg
+	while (len_temp = recv(connect_SOCKET, buf, BUFLEN, 0) > 0) {
+		char *p_end = NULL;
+		reply_code = strtol(buf, &p_end, 10);
+		print_reply_code(reply_code);
+
+		// 221 Service closing control connection.
+		//     Logged out if appropriate.
+		if (reply_code == 221) {
+			break;
+		}
+		// error happen
+		return 0;
+	}
+	memset(buf, 0, BUFLEN);
+
+	return reply_code;
 }
---
+++ ftp_client/reply.h
@@ -0,0 +1,9 @@
+#ifndef _REPLY_INCLUDED
+#define _REPLY_INCLUDED
+
+/* Print full message from server
+ * by reply_code, follow RFC 959
+ */
+void print_reply_code(int reply_code);
+
+#endif
